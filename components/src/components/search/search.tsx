d9ba1193276caef4831afc26d5bfcc563b8fd460
Aleksandr Karapetov <karapet.yo@gmail.com>
2023-03-19 21:30:34 +0300

    feat: add input value save in localStorage

--- components/src/components/search/search.tsx
+++ components/src/components/search/search.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './search.scss';
 
-class Search extends React.Component {
+class Search extends React.Component<object, { value: string }> {
+  constructor(props: object) {
+    super(props);
+    this.state = { value: localStorage.getItem('search') || '' };
+  }
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: event.target.value });
+  };
+  componentWillUnmount(): void {
+    localStorage.setItem('search', this.state.value);
+  }
   render() {
-    return <input className="search_input" type="text" placeholder="search.."></input>;
+    return (
+      <input
+        className="search_input"
+        type="text"
+        placeholder="search.."
+        value={this.state.value}
+        onChange={this.handleChange}
+      ></input>
+    );
   }
 }
 
